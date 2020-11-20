e0dce0c0aed9712e630997e33724f05c36922558
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-11-19 16:23:48 -0800

    add Hero component (desktop styles/light mode)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,24 @@
 import React from "react";
+import headshot from "../../images/headshot.png";
 
 export default function Hero() {
-  return <div></div>;
+  return (
+    <div className="hero wrapper light">
+      <div className="hero-grid1">
+        <h1 className="hero-heading">
+          Hi!
+          <br />
+          I'm Eddie, a freelance Web Developer
+          <br />
+          located in Los Angeles.
+        </h1>
+        <div className="hero-subheading">i make websites ;)</div>
+        <button className="hero-btn">contact me</button>
+      </div>
+
+      <div className="hero-grid2">
+        <img src={headshot} alt="" className="hero-headshot" />
+      </div>
+    </div>
+  );
 }

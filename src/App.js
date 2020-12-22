b1259a76b5e31531e2b8cb5ebb929f4caadb79d1
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-12-21 19:47:19 -0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles/main.scss";
 import Home from "./components/Home/Home";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
